61c3f7977f0c17fed55be9a84df547ffee716293
Kazuki Shibata <k.shibata97@gmail.com>
2020-09-24 20:05:42 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData() {
+    const { data } = await axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      'https://shibata-blog.microcms.io/api/v1/blog',
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: { 'X-API-KEY': '584e178f-5367-4c16-8e58-269a425aa22a' }
+      }
+    )
+    return data
+  }
+}
+</script>
